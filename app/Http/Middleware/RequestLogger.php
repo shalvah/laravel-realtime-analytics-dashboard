c0e3369d1e07c0f2c551abbe9f926d2d2348af1c
shalvah <diakon.ng@gmail.com>
2018-04-28 08:05:48 +0100

    Create RequestLogger middleware

---
+++ app/Http/Middleware/RequestLogger.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\RequestLog;
+use Carbon\Carbon;
+use Closure;
+
+class RequestLogger
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(\Illuminate\Http\Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($request->routeIs('analytics.dashboard')) {
+            return $response;
+        }
+
+        $requestTime = Carbon::createFromTimestamp($_SERVER['REQUEST_TIME']);
+        $request = RequestLog::create([
+            'url' => $request->getPathInfo(),
+            'method' => $request->method(),
+            'response time' => time() - $requestTime->timestamp,
+            'day' => date('l', $requestTime->timestamp),
+            'hour' => $requestTime->hour,
+        ]);
+
+        return $response;
+    }
+}
---
+++ app/Models/RequestLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class RequestLog extends Model
+{
+    protected $guarded = [];
+}
