e2b8e6fb52db77e05db12452711c5c850d4b008e
shalvah <diakon.ng@gmail.com>
2018-04-29 12:09:33 +0100

    Bring laravel Echo into da mix!

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,14 +9,16 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+
+    data: window.analytics
 });
+
+Echo.channel('analytics')
+    .listen('AnalyticsUpdated', (e) => {
+        console.log(e)
+        Object.keys(e.analytics).forEach(stat => {
+            window.analytics[stat] = e.analytics[stat];
+        })
+    });
